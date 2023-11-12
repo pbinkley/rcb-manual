dff9b7d6b2b025e94def1522b8ab4be084ee67f5
Peter Binkley <peter.binkley@ualberta.ca>
2023-11-12 13:14:08 -0700

    Add and run _data/fix.rb

---
+++ _data/fix.rb
@@ -0,0 +1,20 @@
+require 'csv'
+require 'byebug'
+
+input = CSV.parse(File.read('pages.csv'), headers: true, converters: :numeric)
+
+input.each do |row|
+	if row['page'].is_a? Integer
+		row['ia_page'] = row['page']
+	else
+		row['ia_page'] = "n#{row['canvas']}"
+	end
+end
+
+CSV.open("new_pages.csv", "w") do |csv|
+  csv << input.first.headers
+  input.each do |row|
+    csv << row
+  end
+end
+puts 'done'
